a0b22286f7e8d952a13b08628b91ae4e8ae5bae7
dlee67 <dlee67@msudenver.edu>
2018-11-27 18:39:07 -0700

    CommonsSQLitePrac has a working demonstration of how the SQLite could interact with the Android app.

--- CommonsSQLitePrac/app/src/main/java/com/example/bob/commonssqliteprac/MainActivity.java
+++ CommonsSQLitePrac/app/src/main/java/com/example/bob/commonssqliteprac/MainActivity.java
@@ -1,23 +1,76 @@
 package com.example.bob.commonssqliteprac;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.hardware.SensorManager;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper db;
+    String TITLE="title";
+    String VALUE="value";
+    String TABLE="constants";
+    DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.deleteDatabase("stuff.db");
+
+        Log.i("dhl", "Initializing DatabaseHelper.");
+        dbHelper = new DatabaseHelper(this, "stuff.db");
+        dbHelper.close();
+
+        insertStuff();
+        readStuff();
+
         //Wow, the below is actually pretty neat.
         //The below has a lot of potential to decrease a complexity.
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_activity, new MainFragment(), "MainFragment")
-                .commit();
-        db = new DatabaseHelper(getApplicationContext());
-        db.close();
+        //getSupportFragmentManager().beginTransaction()
+        //        .add(R.id.main_fragment, new MainFragment(), "MainFragment")
+        //        .commit();
+    }
+
+    public void insertStuff(){
+
+        Log.i("dhl", "On insert stuff.");
+
+        SQLiteDatabase db  = dbHelper.getWritableDatabase();
+
+        //It seems like all the values associated with SQLite must be wrapped with ContentValues before being inserted.
+        //https://developer.android.com/reference/android/content/ContentValues
+        //More specifically, ContentValues are objects those can be processed via ContentResolver.
+        ContentValues cv=new ContentValues();
+
+        cv.put(TITLE, "Gravity, Death Start I");
+        //The below is an actual constant provided by SensorManager...
+        cv.put(VALUE, SensorManager.GRAVITY_DEATH_STAR_I);
+        db.insert(TABLE, TITLE, cv);
+    }
+
+    public void readStuff(){
+
+        Log.i("dhl", "On read stuff.");
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor result = db.rawQuery("SELECT * FROM constants;", null);
+        for(int index = 0; result.moveToNext(); index++){
+            int id=result.getInt(index);
+            String name=result.getString(index);
+            int inventory=result.getInt(index);
+            Log.i("dhl", "id is: " + id);
+            Log.i("dhl", "name is: " + name);
+            Log.i("dhl", "inventory is: " + inventory);
+        }
+        result.close();
     }
 }
--- CommonsSQLitePrac/app/src/main/java/com/example/bob/commonssqliteprac/MainFragment.java
+++ CommonsSQLitePrac/app/src/main/java/com/example/bob/commonssqliteprac/MainFragment.java
@@ -7,10 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class MainFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
 
-        return inflater.inflate(R.layout.fragment_one, null);
+        //getActivity().getApplicationContext().deleteDatabase("stuff.db");
+        //
+        //DatabaseHelper db;
+        //db = new DatabaseHelper(getActivity());
+        //db.close();
+
+        View mainView = inflater.inflate(R.layout.fragment_one, null);
+        return mainView;
     }
+
 }
