466a43e8040db06303418d0cf16df5e6bffd43b2
dlee67 <dlee67@msudenver.edu>
2019-03-28 00:22:10 -0600

    Moar prac.

--- Go2019/main.go
+++ Go2019/main.go
@@ -2,6 +2,8 @@ package main
 
 //https://tutorialedge.net/golang/golang-mysql-tutorial/
 //Almost complete copy pasta from the above.
+//mysql -u root -p
+//with "root" as the password.
 
 import(
   "fmt"
@@ -25,15 +27,79 @@ func main(){
 
   fmt.Println("MySQL was successfully connected.")
 
-  results, err := db.Query("SELECT name, age FROM person")
-  if err != nil {
+  //READ
+  resultsOne, errOne := db.Query("SELECT * FROM person;")
+  if errOne != nil {
     panic(err.Error())
 	}
 
-  for results.Next(){
+  for resultsOne.Next(){
     var tag Tag
 
-    err = results.Scan(&tag.name, &tag.age)
+    err = resultsOne.Scan(&tag.name, &tag.age)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+    fmt.Println(tag.name)
+  }
+
+  //CREATE
+  db.Query("INSERT INTO person (name, age) VALUES ('Bob', 23);")
+
+  //CREATE
+  db.Query("INSERT INTO person (name, age) VALUES ('Sam', 23);")
+
+  //READ
+  resultsFour, errFour := db.Query("SELECT * FROM person;")
+  if errFour != nil {
+    panic(err.Error())
+	}
+
+  for resultsFour.Next(){
+    var tag Tag
+
+    err = resultsFour.Scan(&tag.name, &tag.age)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+    fmt.Println(tag.name)
+  }
+
+  //DELETE
+  db.Query("DELETE FROM person WHERE name='Bob';")
+
+  //READ
+  resultsSix, errSix := db.Query("SELECT * FROM person;")
+  if errSix != nil {
+    panic(err.Error())
+	}
+
+  for resultsSix.Next(){
+    var tag Tag
+
+    err = resultsSix.Scan(&tag.name, &tag.age)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+    fmt.Println(tag.name)
+  }
+
+  //UPDATE
+  db.Query("UPDATE person SET age=14 WHERE name='Sam';")
+
+  //READ
+  resultsEight, errEight := db.Query("SELECT * FROM person;")
+  if errEight != nil {
+    panic(err.Error())
+	}
+
+  for resultsEight.Next(){
+    var tag Tag
+
+    err = resultsEight.Scan(&tag.name, &tag.age)
 		if err != nil {
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
