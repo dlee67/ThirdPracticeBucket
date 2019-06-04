dfbc2720dbea50cb4b1fa384b28c901918c4914a
dlee67 <dlee67@msudenver.edu>
2019-06-03 19:47:01 -0600

    Moar practice.

--- AngularPrac/my-first-app/src/app/app.component.ts
+++ AngularPrac/my-first-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import 'rxjs/Rx';
-import { Observer } from 'rxjs/Rx';
+import { Observer, Subject } from 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -18,6 +18,19 @@ import { Subscription } from 'rxjs/Subscription';
 export class AppComponent implements OnInit, OnDestroy{
 
   gachiCounterSubscription: Subscription;
+  observer = {
+    next: function(value) {
+      alert(value);
+    },
+    error: function(error){
+      console.log(error);
+    },
+    complete: function() {
+      console.log('Completed');
+    }
+  };
+
+  subject = new Subject();
 
   gymMaster = 'Hey, I think you got the wrong gym!';
   vanSays = "";
@@ -52,9 +65,36 @@ export class AppComponent implements OnInit, OnDestroy{
         this.gachiCounterSubscription.unsubscribe();
       }
     );
+
     // RxJS also provides this API called Subject, which is incredible when it comes to having a component communicating with another.
     // Essentially, forms are clients' submission of data to the server.
     // Angular has its own form called ngForm.
+
+    var observableMapDemo = Observable.interval(1000);
+    var observerMapDemo = {
+      next: function(value) {
+        console.log(value);
+      }
+    }
+    observableMapDemo.map(function(value) {
+      return "Why hello there?"; // So, whatever that is being returned here, is passed to the observer being subscribed.
+    }).throttleTime(2000) // throttleTime() does not behave like sleep. The operation is still happening, but it's not effecting anything.
+    .subscribe(observerMapDemo);
+
+    //Subject remedies such problem by having the next, error, and complete be invokable like methods,
+    //var subject = new Subject();
+    this.subject.subscribe({
+      next: function(value){
+        alert(value);
+      },
+      error: function(error){
+        console.log(error);
+      },
+      complete: function(){
+        console.log('Complete');
+      }
+    });
+    //subject.next("Subject's next function was called."); // and have it be invoked when the programmer wants to.
   }
 
   ngOnDestroy(){
@@ -63,5 +103,16 @@ export class AppComponent implements OnInit, OnDestroy{
 
   getColor(){
     return "red";
+  }
+
+  //The differences between those two are clear.
+  gachiClick(){
+    Observable.create(function(obs) {
+      obs.next("Are you embarassing me, huh?");
+      obs.complete("Gachi complete.");
+    }).subscribe(this.observer);
+  }
+  gachiSubject(){
+    this.subject.next("Aniki subject.");
   }
 }
